8868b71ac91a8110eb5aa88ca3ef28cf7873ad6f
quintabcn <mmqok@outloo.com>
2021-01-10 21:55:19 -0300

    crud mvc 1

--- CRUD-MVC/index.php
+++ CRUD-MVC/index.php
@@ -4,12 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 
-    <style>
-        h1{
-        text-align: center;
-        }
-    </style>
 </head>
 <body>
     <h1>MODELO VISTA CONTROLADOR</h1>
